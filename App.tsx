a91442930faec6950956ce8c671588bc91afdcf1
Marlon Marques <marlon.belohd@gmail.com>
2022-07-25 19:39:50 -0300

    feat: initial commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.sectionContainer}>
+      <StatusBar barStyle={'dark-content'} />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Voy!</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1,
+  },
+  titleContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
