c689db9f2a04cbf0677dc23ad69809602c03c0d1
Marlon Marques <marlon.belohd@gmail.com>
2023-06-25 17:58:42 -0300

    test(data/remotesociallogin): realize social login calling httpPostClient with correct client id

--- tests/data/useCases/remoteSocialLogin.spec.ts
+++ tests/data/useCases/remoteSocialLogin.spec.ts
@@ -16,12 +16,24 @@ describe('Data: RemoteSocialLogin', () => {
       url + '?provider=' + Social.FACEBOOK.toLowerCase(),
     );
   });
+
+  test('should realize social login calling httpPostClient with correct client id', () => {
+    const httpClient = new HttpClientSpy();
+    const url = makeUrl();
+    const clientId = 'any_clientId';
+    const sut = new RemoteSocialLogin(url, httpClient);
+    sut.login(clientId, Social.FACEBOOK);
+
+    expect(httpClient.body).toEqual({
+      client_id: clientId,
+    });
+  });
 });
 
 class RemoteSocialLogin implements SocialLogin {
   constructor(readonly url: string, readonly httpPostClient: HttpPostClient) {}
   login(clientId: string, socialType: Social): Promise<string> {
     const url = this.url + '?provider=' + socialType.toLowerCase();
-    this.httpPostClient.post({ url: url });
+    this.httpPostClient.post({ url: url, body: { client_id: clientId } });
   }
 }
