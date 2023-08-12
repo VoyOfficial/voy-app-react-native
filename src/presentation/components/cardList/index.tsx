16b8a450e28b676174b20877ccba4538789912dd
Marlon Marques <marlon.belohd@gmail.com>
2023-08-12 17:22:28 -0300

    style(presentation/cardlist): add style in component

--- src/presentation/components/cardList/index.tsx
+++ src/presentation/components/cardList/index.tsx
@@ -19,13 +19,83 @@ const CardList = ({
   reviews,
 }: Props) => {
   return (
-    <View>
-      <Image testID="image_of_place_id" source={{ uri: imageUrl }} />
-      <Text testID="title_id">{title}</Text>
-      <Text testID="location_id">{location}</Text>
-      <Text testID="distance_of_local_id">{myDistanceOfLocal}</Text>
-      <Text testID="rating_comments_id">{ratingComments}</Text>
-      <Text testID="reviews_id">{reviews}</Text>
+    <View
+      style={{
+        backgroundColor: '#FFF',
+        flexDirection: 'row',
+        borderRadius: 15,
+        height: 115,
+      }}
+    >
+      <Image
+        style={{
+          width: 115,
+          height: 115,
+          borderTopLeftRadius: 15,
+          borderBottomLeftRadius: 15,
+        }}
+        testID="image_of_place_id"
+        source={{ uri: imageUrl }}
+      />
+      <View style={{ margin: 12, justifyContent: 'space-around' }}>
+        <Text
+          style={{
+            color: '#212121',
+            fontSize: 17,
+            fontWeight: '400',
+            lineHeight: 21.25,
+          }}
+          testID="title_id"
+        >
+          {title}
+        </Text>
+        <Text
+          style={{
+            fontSize: 13,
+            fontWeight: '400',
+            lineHeight: 16.25,
+            color: '#AEAEAE',
+          }}
+          testID="location_id"
+        >
+          {location}
+        </Text>
+        <Text
+          style={{
+            fontSize: 13,
+            fontWeight: '400',
+            lineHeight: 16.25,
+            color: '#AEAEAE',
+          }}
+          testID="distance_of_local_id"
+        >
+          {myDistanceOfLocal}
+        </Text>
+        <View style={{ flexDirection: 'row' }}>
+          <Text
+            style={{
+              color: '#212121',
+              fontSize: 13,
+              fontWeight: '400',
+              lineHeight: 16.25,
+            }}
+            testID="reviews_id"
+          >
+            {reviews}
+          </Text>
+          <Text
+            style={{
+              color: '#AEAEAE',
+              fontSize: 13,
+              fontWeight: '400',
+              lineHeight: 16.25,
+            }}
+            testID="rating_comments_id"
+          >
+            {`(${ratingComments})`}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
