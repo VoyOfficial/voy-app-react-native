92a5130395fd2f06402dde9a1ccdb2cdd4499847
Marlon Marques <marlon.belohd@gmail.com>
2023-09-26 09:23:23 -0300

    test(presentation/cardlist): press button "Ver todos" with success

--- tests/presentation/components/cardList/index.spec.tsx
+++ tests/presentation/components/cardList/index.spec.tsx
@@ -1,33 +1,50 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { faker } from '@faker-js/faker';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 
 describe('Components: CardList', () => {
   test('should show title of CardList with success', () => {
     const title = faker.random.word();
-    const { getByTestId } = render(<CardList title={title} />);
+    const { getByTestId } = render(
+      <CardList title={title} seeAll={() => {}} />,
+    );
 
     expect(getByTestId('title_id').props.children).toEqual(title);
   });
 
   test('should show button "Ver todos" correctly', () => {
     const title = faker.random.word();
-    const { getByTestId } = render(<CardList title={title} />);
+    const { getByTestId } = render(
+      <CardList title={title} seeAll={() => {}} />,
+    );
 
     expect(getByTestId('see_all_id').props.children).toEqual('Ver todos');
+  });
+
+  test('should press button "Ver todos" with success', () => {
+    const title = faker.random.word();
+    const seeAll = jest.fn();
+    const { getByTestId } = render(<CardList title={title} seeAll={seeAll} />);
+
+    fireEvent.press(getByTestId('see_all_button_id'));
+
+    expect(seeAll).toHaveBeenCalledTimes(1);
   });
 });
 
 type Props = {
   title: string;
+  seeAll: () => void;
 };
 
-const CardList = ({ title }: Props) => {
+const CardList = ({ title, seeAll }: Props) => {
   return (
     <View>
       <Text testID="title_id">{title}</Text>
-      <Text testID="see_all_id">{'Ver todos'}</Text>
+      <TouchableOpacity testID="see_all_button_id" onPress={seeAll}>
+        <Text testID="see_all_id">{'Ver todos'}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
