6cb7fe11f495984f8eebbdfe70a0176d96689b9e
Marlon Marques <marlon.belohd@gmail.com>
2023-10-01 15:44:07 -0300

    test(presentation/placedetails): show description with success

--- tests/presentation/placeDetails/index.spec.tsx
+++ tests/presentation/placeDetails/index.spec.tsx
@@ -6,20 +6,33 @@ import { faker } from '@faker-js/faker';
 describe('Presentation: PlaceDetails', () => {
   test('should show title with success', () => {
     const title = faker.random.word();
-    const { getByTestId } = render(<PlaceDetails title={title} />);
+    const { getByTestId } = render(
+      <PlaceDetails title={title} description="" />,
+    );
 
     expect(getByTestId('title_id').props.children).toEqual(title);
+  });
+
+  test('should show description with success', () => {
+    const description = faker.lorem.paragraph();
+    const { getByTestId } = render(
+      <PlaceDetails title="" description={description} />,
+    );
+
+    expect(getByTestId('description_id').props.children).toEqual(description);
   });
 });
 
 type Props = {
   title: string;
+  description: string;
 };
 
-const PlaceDetails = ({ title }: Props) => {
+const PlaceDetails = ({ title, description }: Props) => {
   return (
     <View>
       <Text testID="title_id">{title}</Text>
+      <Text testID="description_id">{description}</Text>
     </View>
   );
 };
