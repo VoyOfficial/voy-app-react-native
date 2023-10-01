8c8e33770367a65e847c6c10fb61206e4fa4577b
Marlon Marques <marlon.belohd@gmail.com>
2023-10-01 16:16:13 -0300

    test(presentation/placedetails): show my distance of local with success

--- tests/presentation/placeDetails/index.spec.tsx
+++ tests/presentation/placeDetails/index.spec.tsx
@@ -7,7 +7,12 @@ describe('Presentation: PlaceDetails', () => {
   test('should show title with success', () => {
     const title = faker.random.word();
     const { getByTestId } = render(
-      <PlaceDetails title={title} description="" />,
+      <PlaceDetails
+        title={title}
+        description=""
+        location={''}
+        myDistanceOfLocal={''}
+      />,
     );
 
     expect(getByTestId('title_id').props.children).toEqual(title);
@@ -16,7 +21,12 @@ describe('Presentation: PlaceDetails', () => {
   test('should show description with success', () => {
     const description = faker.lorem.paragraph();
     const { getByTestId } = render(
-      <PlaceDetails title="" description={description} />,
+      <PlaceDetails
+        title=""
+        description={description}
+        location={''}
+        myDistanceOfLocal={''}
+      />,
     );
 
     expect(getByTestId('description_id').props.children).toEqual(description);
@@ -26,10 +36,33 @@ describe('Presentation: PlaceDetails', () => {
     const description = faker.lorem.paragraph();
     const location = faker.address.cityName();
     const { getByTestId } = render(
-      <PlaceDetails title="" description={description} location={location} />,
+      <PlaceDetails
+        title=""
+        description={description}
+        location={location}
+        myDistanceOfLocal={''}
+      />,
     );
 
     expect(getByTestId('location_id').props.children).toEqual(location);
+  });
+
+  test('should show my distance of local with success', () => {
+    const description = faker.lorem.paragraph();
+    const location = faker.address.cityName();
+    const myDistanceOfLocal = faker.random.numeric(4);
+    const { getByTestId } = render(
+      <PlaceDetails
+        title=""
+        description={description}
+        location={location}
+        myDistanceOfLocal={myDistanceOfLocal}
+      />,
+    );
+
+    expect(getByTestId('distance_of_local_id').props.children).toEqual(
+      myDistanceOfLocal,
+    );
   });
 });
 
@@ -37,14 +70,21 @@ type Props = {
   title: string;
   description: string;
   location: string;
+  myDistanceOfLocal: string;
 };
 
-const PlaceDetails = ({ title, description, location }: Props) => {
+const PlaceDetails = ({
+  title,
+  description,
+  location,
+  myDistanceOfLocal,
+}: Props) => {
   return (
     <View>
       <Text testID="title_id">{title}</Text>
       <Text testID="description_id">{description}</Text>
       <Text testID="location_id">{location}</Text>
+      <Text testID="distance_of_local_id">{myDistanceOfLocal}</Text>
     </View>
   );
 };
