e4fee6bf4cb804d5a4760f7db4269349778aeb87
Sting Silva <stingufc@gmail.com>
2023-10-23 11:36:26 -0300

    test: created list recommendation component

---
+++ tests/presentation/recommendation/components/listRecommendation/view.spec.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { fireEvent, render, waitFor } from '@testing-library/react-native';
+import placeListFactory from '../../../helpers/placeListFactory';
+import { ListRecommendation } from '../../../../../src/presentation/recommentation/components';
+placeListFactory;
+describe('Components: ListRecommendation', () => {
+  test('should render list of recommendations', () => {
+    const { sut } = makeSut({});
+
+    expect(sut.getByText(/Recomendações/i)).toBeTruthy();
+    expect(sut.getAllByTestId('title_id')[0]).toBeTruthy();
+  });
+
+  test('should list the correct quantity of items', () => {
+    const { sut, recommendations } = makeSut({});
+
+    const lengthList = recommendations.length;
+
+    const list = sut.getByTestId('recommendation-list');
+
+    expect(list.props.data.length).toBe(lengthList);
+  });
+
+  test('should call see all recommendations function', async () => {
+    const handleSeeAll = jest.fn();
+    const { sut } = makeSut({
+      onSeeAll: handleSeeAll,
+    });
+
+    const seeAllBtn = sut.getByTestId('onpress-see-all');
+
+    waitFor(() => fireEvent.press(seeAllBtn));
+
+    expect(handleSeeAll).toBeCalled();
+  });
+
+  const makeSut = ({ onSeeAll = jest.fn() }) => {
+    const recommendations = placeListFactory(3);
+
+    const sut = render(
+      <ListRecommendation
+        onSeeAll={onSeeAll}
+        recommendations={recommendations}
+      />,
+    );
+
+    return { sut, recommendations };
+  };
+});
