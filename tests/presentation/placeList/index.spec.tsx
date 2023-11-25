2c58be9048bf0e0594381f8e8fdd613e38f680e6
Marlon Marques <marlon.belohd@gmail.com>
2023-11-25 18:23:07 -0300

    test(presentation/placelist): call favorite function when press the save button

--- tests/presentation/placeList/index.spec.tsx
+++ tests/presentation/placeList/index.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import CardList, { Place } from '../../../src/presentation/components/cardList';
 import { getPlaceDetails } from '../components/cardList/index.spec';
 import placeListFactory from '../helpers/placeListFactory';
@@ -8,7 +8,9 @@ import placeListFactory from '../helpers/placeListFactory';
 describe('Presentation: PlaceList', () => {
   test('should list places correctly', () => {
     const list = placeListFactory(5);
-    const { getByTestId } = render(<PlaceList list={list} />);
+    const { getByTestId } = render(
+      <PlaceList list={list} favorite={() => {}} />,
+    );
 
     list.forEach((place, index) => {
       const {
@@ -31,36 +33,49 @@ describe('Presentation: PlaceList', () => {
 
   test('should not show title', () => {
     const list = placeListFactory(5);
-    const { getByTestId } = render(<PlaceList list={list} />);
+    const { getByTestId } = render(
+      <PlaceList list={list} favorite={() => {}} />,
+    );
 
     expect(getByTestId('title_id').props.children).toEqual('');
   });
 
   test('should not show see all button', () => {
     const list = placeListFactory(5);
-    const { queryByTestId } = render(<PlaceList list={list} />);
+    const { queryByTestId } = render(
+      <PlaceList list={list} favorite={() => {}} />,
+    );
 
     expect(queryByTestId('see_all_button_id')).not.toBeTruthy();
+  });
+
+  test('should call favorite function when press the save button', () => {
+    const favoriteSpy = jest.fn();
+    const list = placeListFactory(5);
+    const { getByTestId } = render(
+      <PlaceList list={list} favorite={favoriteSpy} />,
+    );
+
+    fireEvent.press(getByTestId('save_button_1_id'));
+
+    expect(favoriteSpy).toHaveBeenCalledTimes(1);
   });
 });
 
 type Props = {
   list: Array<Place>;
+  favorite: () => void;
 };
 
-const PlaceList = ({ list }: Props) => {
+const PlaceList = ({ list, favorite }: Props) => {
   return (
     <View>
       <CardList
         title={''}
         showSeeAllButton={false}
-        seeAll={function (): void {
-          throw new Error('Function not implemented.');
-        }}
+        seeAll={() => {}}
         placeList={list}
-        favorite={function (): void {
-          throw new Error('Function not implemented.');
-        }}
+        favorite={favorite}
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         showMoreDetails={function (place: Place): void {
           throw new Error('Function not implemented.');
