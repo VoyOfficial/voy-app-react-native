8b4d9f90d3b96ec404e57469d951864dd6d6b509
Marlon Marques <marlon.belohd@gmail.com>
2023-11-25 20:34:15 -0300

    test(presentation/placelist): refactor tests with makeSut function

--- tests/presentation/placeList/index.spec.tsx
+++ tests/presentation/placeList/index.spec.tsx
@@ -6,10 +6,10 @@ import PlaceList from '../../../src/presentation/placeList';
 
 describe('Presentation: PlaceList', () => {
   test('should list places correctly', () => {
-    const list = placeListFactory(5);
-    const { getByTestId } = render(
-      <PlaceList list={list} favorite={() => {}} showMoreDetails={() => {}} />,
-    );
+    const {
+      sut: { getByTestId },
+      list,
+    } = makeSut();
 
     list.forEach((place, index) => {
       const {
@@ -31,33 +31,26 @@ describe('Presentation: PlaceList', () => {
   });
 
   test('should not show title', () => {
-    const list = placeListFactory(5);
-    const { getByTestId } = render(
-      <PlaceList list={list} favorite={() => {}} showMoreDetails={() => {}} />,
-    );
+    const {
+      sut: { getByTestId },
+    } = makeSut();
 
     expect(getByTestId('title_id').props.children).toEqual('');
   });
 
   test('should not show see all button', () => {
-    const list = placeListFactory(5);
-    const { queryByTestId } = render(
-      <PlaceList list={list} favorite={() => {}} showMoreDetails={() => {}} />,
-    );
+    const {
+      sut: { queryByTestId },
+    } = makeSut();
 
     expect(queryByTestId('see_all_button_id')).not.toBeTruthy();
   });
 
   test('should call favorite function when press the save button', () => {
-    const favoriteSpy = jest.fn();
-    const list = placeListFactory(5);
-    const { getByTestId } = render(
-      <PlaceList
-        list={list}
-        favorite={favoriteSpy}
-        showMoreDetails={() => {}}
-      />,
-    );
+    const {
+      sut: { getByTestId },
+      favoriteSpy,
+    } = makeSut();
 
     fireEvent.press(getByTestId('save_button_1_id'));
 
@@ -65,19 +58,31 @@ describe('Presentation: PlaceList', () => {
   });
 
   test('should call showMoreDetails function correctly when press the show more details button', () => {
-    const showMoreDetails = jest.fn();
-    const list = placeListFactory(5);
-    const { getByTestId } = render(
-      <PlaceList
-        list={list}
-        favorite={() => {}}
-        showMoreDetails={showMoreDetails}
-      />,
-    );
+    const {
+      sut: { getByTestId },
+      showMoreDetailsSpy,
+      list,
+    } = makeSut();
 
     fireEvent.press(getByTestId('list_card_1_id'));
 
-    expect(showMoreDetails).toHaveBeenCalledTimes(1);
-    expect(showMoreDetails).toHaveBeenCalledWith(list[1]);
+    expect(showMoreDetailsSpy).toHaveBeenCalledTimes(1);
+    expect(showMoreDetailsSpy).toHaveBeenCalledWith(list[1]);
   });
 });
+
+const makeSut = () => {
+  const showMoreDetailsSpy = jest.fn();
+  const favoriteSpy = jest.fn();
+
+  const list = placeListFactory(5);
+  const sut = render(
+    <PlaceList
+      list={list}
+      favorite={favoriteSpy}
+      showMoreDetails={showMoreDetailsSpy}
+    />,
+  );
+
+  return { sut, list, showMoreDetailsSpy, favoriteSpy };
+};
