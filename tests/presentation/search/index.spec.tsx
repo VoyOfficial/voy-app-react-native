ebcffeab641c4a7fbb2ab206a0f1c30e705d573f
Marlon Marques <marlon.belohd@gmail.com>
2023-12-16 17:09:11 -0300

    test(presentation/search): show the search input value successfully

--- tests/presentation/search/index.spec.tsx
+++ tests/presentation/search/index.spec.tsx
@@ -4,16 +4,27 @@ import { render } from '@testing-library/react-native';
 
 describe('Presentation: Search', () => {
   test('should show input of search with success', () => {
-    const { getByTestId } = render(<Search />);
+    const { getByTestId } = render(<Search search="" />);
 
     expect(getByTestId('search_input_id')).toBeTruthy();
   });
+
+  test('should show the search input value successfully', () => {
+    const search = 'Cafeteria';
+    const { getByTestId } = render(<Search search={search} />);
+
+    expect(getByTestId('search_input_id').props.value).toEqual('Cafeteria');
+  });
 });
 
-const Search = () => {
+type Props = {
+  search: string;
+};
+
+const Search = ({ search }: Props) => {
   return (
     <View>
-      <TextInput testID="search_input_id" />
+      <TextInput testID="search_input_id" value={search} />
     </View>
   );
 };
