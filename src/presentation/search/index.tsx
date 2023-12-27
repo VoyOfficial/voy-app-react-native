3a54fd92ad7c7b88e79bcb66e46691fd96dc39f9
Marlon Marques <marlon.belohd@gmail.com>
2023-12-27 17:26:14 -0300

    style(presentation/search): add style to search text input

--- src/presentation/search/index.tsx
+++ src/presentation/search/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TextInput, View } from 'react-native';
+import Icon from '../assets/fonts/Voy';
 
 type Props = {
   searchValue: string;
@@ -9,14 +10,35 @@ type Props = {
 
 const Search = ({ searchValue, changeSearch, searchTo }: Props) => {
   return (
-    <View>
-      <TextInput
-        testID="search_input_id"
-        placeholder="Pesquisar lugares..."
-        value={searchValue}
-        onChangeText={changeSearch}
-        onSubmitEditing={() => searchTo(searchValue)}
-      />
+    <View style={{ paddingTop: 80 }}>
+      <View>
+        <TextInput
+          testID="search_input_id"
+          placeholder="Pesquisar lugares..."
+          value={searchValue}
+          onChangeText={changeSearch}
+          onSubmitEditing={() => searchTo(searchValue)}
+          style={{
+            backgroundColor: '#FFFFFF',
+            paddingVertical: 11,
+            paddingLeft: 16,
+            borderRadius: 13,
+          }}
+        />
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingRight: 8.5,
+          }}
+        >
+          <Icon testID="search_id" name="search" size={22.5} color="#B3B3B3" />
+        </View>
+      </View>
     </View>
   );
 };
