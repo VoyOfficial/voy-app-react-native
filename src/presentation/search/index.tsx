f3e1412357c72290b6ac0533379c0e006d24d859
Marlon Marques <marlon.belohd@gmail.com>
2024-01-21 18:34:42 -0300

    style(presentation/search): move style to styles file

--- src/presentation/search/index.tsx
+++ src/presentation/search/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import { TextInput, TouchableOpacity, View } from 'react-native';
-import Icon from '../assets/fonts/Voy';
 import CardList, { Place } from '../components/cardList';
 import FilterModal from './modal';
+import {
+  CardListWrapper,
+  Container,
+  FilterButton,
+  FilterButtonWrapper,
+  FilterIcon,
+  FilterModalWrapper,
+  HeaderWrapper,
+  SearchIcon,
+  SearchIconWrapper,
+  SearchInput,
+  SearchInputWrapper,
+} from './styles';
 
 type Props = {
   searchValue: string;
@@ -22,62 +33,28 @@ const Search = ({
   filter,
 }: Props) => {
   return (
-    <View style={{ paddingTop: 80 }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          width: '100%',
-          justifyContent: 'center',
-          alignItems: 'center',
-          paddingHorizontal: 20,
-        }}
-      >
-        <View style={{ paddingRight: 21 }}>
-          <TouchableOpacity testID="filter_button_id" onPress={filter}>
-            <Icon
-              testID="filter_id"
-              name="filter"
-              size={22.5}
-              color="#212121"
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={{ flex: 1 }}>
-          <TextInput
+    <Container>
+      <HeaderWrapper>
+        <FilterButtonWrapper>
+          <FilterButton testID="filter_button_id" onPress={filter}>
+            <FilterIcon testID="filter_id" name="filter" />
+          </FilterButton>
+        </FilterButtonWrapper>
+        <SearchInputWrapper>
+          <SearchInput
             testID="search_input_id"
             placeholder="Pesquisar lugares..."
             value={searchValue}
             onChangeText={changeSearch}
             onSubmitEditing={() => searchTo(searchValue)}
-            style={{
-              backgroundColor: '#FFFFFF',
-              paddingVertical: 11,
-              paddingLeft: 16,
-              borderRadius: 13,
-            }}
           />
-          <View
-            style={{
-              position: 'absolute',
-              top: 0,
-              right: 0,
-              bottom: 0,
-              justifyContent: 'center',
-              alignItems: 'center',
-              paddingRight: 8.5,
-            }}
-          >
-            <Icon
-              testID="search_id"
-              name="search"
-              size={22.5}
-              color="#B3B3B3"
-            />
-          </View>
-        </View>
-      </View>
+          <SearchIconWrapper>
+            <SearchIcon testID="search_id" name="search" />
+          </SearchIconWrapper>
+        </SearchInputWrapper>
+      </HeaderWrapper>
       {searchValue !== '' && (
-        <View style={{ marginHorizontal: 20 }}>
+        <CardListWrapper>
           <CardList
             title={''}
             seeAllBy={''}
@@ -87,20 +64,10 @@ const Search = ({
             showSeeAllButton={false}
             showMoreDetails={() => {}}
           />
-        </View>
+        </CardListWrapper>
       )}
       {showFilterOptions && (
-        <View
-          style={{
-            marginHorizontal: 20,
-            marginTop: 20,
-            position: 'absolute',
-            top: 120,
-            bottom: 0,
-            left: 0,
-            right: 0,
-          }}
-        >
+        <FilterModalWrapper>
           <FilterModal
             filterOptions={{
               filterBy: {
@@ -129,16 +96,12 @@ const Search = ({
             selectOrder={() => {}}
             showOrderList={true}
             showFilterList={true}
-            changeShowOfOrderList={function (): void {
-              throw new Error('Function not implemented.');
-            }}
-            changeShowOfFilterList={function (): void {
-              throw new Error('Function not implemented.');
-            }}
+            changeShowOfOrderList={() => {}}
+            changeShowOfFilterList={() => {}}
           />
-        </View>
+        </FilterModalWrapper>
       )}
-    </View>
+    </Container>
   );
 };
 
