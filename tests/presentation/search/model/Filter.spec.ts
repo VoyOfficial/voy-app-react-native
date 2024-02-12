df92426f56f3a476134de6e88bcee2979cbe010c
Marlon Marques <marlon.belohd@gmail.com>
2024-02-12 17:26:19 -0300

    test(presentation/model): update options correctly when to call selectFilter

--- tests/presentation/search/model/Filter.spec.ts
+++ tests/presentation/search/model/Filter.spec.ts
@@ -1,32 +1,46 @@
-const options = {
-  filterBy: {
-    label: 'Filtrar por',
-    list: [
-      { id: 1, label: 'Restaurantes', selected: false },
-      { id: 2, label: 'Cafeterias', selected: false },
-      { id: 3, label: 'Entretenimento', selected: false },
-      { id: 4, label: 'Hotéis', selected: false },
-      { id: 5, label: 'Lazer', selected: false },
-      { id: 6, label: 'Esportes', selected: false },
-      { id: 7, label: 'Vida noturna', selected: false },
-    ],
-  },
-  orderBy: {
-    label: 'Ordernar por',
-    list: [
-      { id: 1, label: 'Mais avaliados' },
-      { id: 2, label: 'Mais comentados' },
-      { id: 3, label: 'Distância' },
-    ],
-    selected: { id: 1, label: 'Mais avaliados' },
-  },
+const getOptions = () => {
+  return {
+    filterBy: {
+      label: 'Filtrar por',
+      list: [
+        { id: 1, label: 'Restaurantes', selected: false },
+        { id: 2, label: 'Cafeterias', selected: false },
+        { id: 3, label: 'Entretenimento', selected: false },
+        { id: 4, label: 'Hotéis', selected: false },
+        { id: 5, label: 'Lazer', selected: false },
+        { id: 6, label: 'Esportes', selected: false },
+        { id: 7, label: 'Vida noturna', selected: false },
+      ],
+    },
+    orderBy: {
+      label: 'Ordernar por',
+      list: [
+        { id: 1, label: 'Mais avaliados' },
+        { id: 2, label: 'Mais comentados' },
+        { id: 3, label: 'Distância' },
+      ],
+      selected: { id: 1, label: 'Mais avaliados' },
+    },
+  };
 };
 
 describe('Model: Filter', () => {
   test('should start options correctly when initialize the Filter', () => {
+    const options = getOptions();
     const sut = new Filter(options);
 
     expect(sut.options).toEqual(options);
+  });
+
+  test('should update options correctly when to call selectFilter function', () => {
+    const sut = new Filter(getOptions());
+
+    const optionSelected = sut.options.filterBy.list[4];
+    expect(optionSelected.selected).toEqual(false);
+
+    sut.selectFilter(optionSelected);
+
+    expect(optionSelected.selected).toEqual(true);
   });
 });
 
@@ -42,7 +56,7 @@ type Options = {
   };
 };
 
-import { observable } from 'mobx';
+import { action, observable } from 'mobx';
 
 class Filter {
   @observable options: Options = {
@@ -61,6 +75,15 @@ class Filter {
   };
 
   constructor(options: Options) {
-    this.options = options;
+    this.options = Object.assign({}, options);
   }
+
+  @action selectFilter = (filter: {
+    id: number;
+    label: string;
+    selected: boolean;
+  }) => {
+    const index = this.options.filterBy.list.indexOf(filter);
+    this.options.filterBy.list[index].selected = true;
+  };
 }
