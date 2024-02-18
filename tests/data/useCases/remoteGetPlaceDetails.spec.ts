8bd3059de92c51024add6b0f4010f72bfe187901
Marlon Marques <marlon.belohd@gmail.com>
2024-02-18 17:23:46 -0300

    test(data/remotegetplacedetails): get with httpGetClient returning the place details with success

--- tests/data/useCases/remoteGetPlaceDetails.spec.ts
+++ tests/data/useCases/remoteGetPlaceDetails.spec.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { PlaceDetailsModel } from '~/domain/models';
 import { GetPlaceDetails } from '~/domain/useCases';
-import { HttpGetClient } from '~/data/http';
+import { HttpGetClient, HttpStatusCode } from '~/data/http';
 import { makeUrl } from '../helpers/testFactories';
 import { HttpClientSpy } from '../http/httpClientSpy';
 
@@ -16,6 +16,60 @@ describe('Data: RemoteGetPlaceDetails', () => {
 
     expect(httpClient.url).toEqual(`${url}?id=${id}`);
   });
+
+  test('should get with httpGetClient returning the place details with success', async () => {
+    const placeDetailsResponse = {
+      title: faker.company.name(),
+      amountOfReviews: faker.datatype.number().toString(),
+      businessHoursSummary: {
+        sunday: {
+          start: '08:00',
+          end: '12:00',
+        },
+        monday: {
+          start: '08:00',
+          end: '12:00',
+        },
+        tuesday: {
+          start: '08:00',
+          end: '12:00',
+        },
+        wednesday: {
+          start: '08:00',
+          end: '12:00',
+        },
+        thursday: {
+          start: '08:00',
+          end: '12:00',
+        },
+        friday: {
+          start: '08:00',
+          end: '12:00',
+        },
+        saturday: {
+          start: '08:00',
+          end: '12:00',
+        },
+      },
+      contact: faker.phone.number(),
+      description: faker.lorem.paragraph(),
+      distance: faker.datatype.number().toString(),
+      fullLocation: faker.address.streetAddress(),
+      location: faker.address.cityName(),
+      photoOfReviewProfiles: [faker.image.imageUrl()],
+      rating: faker.datatype.number().toString(),
+    };
+    const url = makeUrl();
+    const httpClient = new HttpClientSpy();
+    httpClient.completeWithSuccess(HttpStatusCode.ok, placeDetailsResponse);
+
+    const sut = new RemoteGetPlaceDetails(url, httpClient);
+
+    const id = faker.datatype.uuid();
+    const placeDetails = await sut.get(id);
+
+    expect(placeDetails).toEqual(placeDetailsResponse);
+  });
 });
 
 class RemoteGetPlaceDetails implements GetPlaceDetails {
@@ -24,47 +78,15 @@ class RemoteGetPlaceDetails implements GetPlaceDetails {
     private readonly httpGetClient: HttpGetClient,
   ) {}
   get = async (id: string): Promise<PlaceDetailsModel> => {
-    this.httpGetClient.get({ url: `${this.url}?id=${id}` });
-    return {
-      amountOfReviews: '',
-      businessHoursSummary: {
-        sunday: {
-          start: '',
-          end: '',
-        },
-        monday: {
-          start: '',
-          end: '',
-        },
-        tuesday: {
-          start: '',
-          end: '',
-        },
-        wednesday: {
-          start: '',
-          end: '',
-        },
-        thursday: {
-          start: '',
-          end: '',
-        },
-        friday: {
-          start: '',
-          end: '',
-        },
-        saturday: {
-          start: '',
-          end: '',
-        },
-      },
-      contact: '',
-      description: '',
-      distance: '',
-      fullLocation: '',
-      location: '',
-      photoOfReviewProfiles: [''],
-      rating: '',
-      title: '',
-    };
+    const { statusCode, body } = await this.httpGetClient.get({
+      url: `${this.url}?id=${id}`,
+    });
+
+    switch (statusCode) {
+      case HttpStatusCode.ok:
+        return body;
+      default:
+        throw new Error(`error`);
+    }
   };
 }
