c9dc25dea189eef44d94927c1e72e800781934da
Marlon Marques <marlon.belohd@gmail.com>
2024-03-02 16:59:10 -0300

    test(presentation/useplacedetails): refactor tests with makeSut

--- tests/presentation/placeDetails/usePlaceDetails.spec.tsx
+++ tests/presentation/placeDetails/usePlaceDetails.spec.tsx
@@ -64,108 +64,76 @@ export class GetPlaceDetailsFake implements GetPlaceDetails {
 
 describe('Presentation: usePlaceDetails', () => {
   test('should update the backgroundImage correctly when call pressSummaryImageFromGallery function', async () => {
-    const { result } = renderHook(() =>
-      usePlaceDetails({
-        gallerySummaryImages: [''],
-        getPlaceDetails: new GetPlaceDetailsFake(),
-        id: '',
-      }),
-    );
+    const { result } = makeSut('', ['']);
 
     expect(result.current.backgroundImage).toEqual('');
 
     const image = faker.image.imageUrl();
 
-    result.current.pressSummaryImageFromGallery(image, false);
-
     await waitFor(() => {
+      result.current.pressSummaryImageFromGallery(image, false);
       expect(result.current.backgroundImage).toEqual(image);
     });
   });
 
   test('should update isOpenImagesGallery to true when call pressSummaryImageFromGallery with showInGallery true', async () => {
-    const { result } = renderHook(() =>
-      usePlaceDetails({
-        gallerySummaryImages: [''],
-        getPlaceDetails: new GetPlaceDetailsFake(),
-        id: '',
-      }),
-    );
+    const { result } = makeSut('', ['']);
 
     expect(result.current.isOpenImagesGallery).toEqual(false);
 
     const image = faker.image.imageUrl();
 
     const showInGallery = true;
-    result.current.pressSummaryImageFromGallery(image, showInGallery);
 
     await waitFor(() => {
+      result.current.pressSummaryImageFromGallery(image, showInGallery);
       expect(result.current.isOpenImagesGallery).toEqual(true);
     });
   });
 
   test('should update isOpenImagesGallery to false when call closeImagesGallery', async () => {
-    const { result } = renderHook(() =>
-      usePlaceDetails({
-        gallerySummaryImages: [''],
-        getPlaceDetails: new GetPlaceDetailsFake(),
-        id: '',
-      }),
-    );
+    const { result } = makeSut('', ['']);
 
     const image = faker.image.imageUrl();
 
     const showInGallery = true;
-    result.current.pressSummaryImageFromGallery(image, showInGallery);
 
     await waitFor(() => {
+      result.current.pressSummaryImageFromGallery(image, showInGallery);
       expect(result.current.isOpenImagesGallery).toEqual(true);
     });
 
-    result.current.closeImagesGallery();
-
     await waitFor(() => {
+      result.current.closeImagesGallery();
       expect(result.current.isOpenImagesGallery).toEqual(false);
     });
   });
 
   test('should update backgroundImage when initialize', async () => {
     const backgroundImage = faker.image.imageUrl();
-    const { result } = renderHook(() =>
-      usePlaceDetails({
-        gallerySummaryImages: [faker.image.imageUrl()],
-        getPlaceDetails: new GetPlaceDetailsFake(),
-        id: '',
-      }),
-    );
+    const { result } = makeSut('', [backgroundImage]);
 
-    expect(result.current.backgroundImage).toEqual(backgroundImage);
+    await waitFor(() => {
+      expect(result.current.backgroundImage).toEqual(backgroundImage);
+    });
   });
 
   test('should call get of GetPlaceDetails correctly when initialize', async () => {
     const getPlaceDetails = new GetPlaceDetailsFake();
     const id = 'any_id';
-    renderHook(() =>
-      usePlaceDetails({
-        gallerySummaryImages: [faker.image.imageUrl()],
-        getPlaceDetails,
-        id,
-      }),
-    );
 
-    expect(getPlaceDetails.id).toEqual(id);
+    makeSut(id, [faker.image.imageUrl()], getPlaceDetails);
+
+    await waitFor(() => {
+      expect(getPlaceDetails.id).toEqual(id);
+    });
   });
 
   test('should get place details with success when initialize', async () => {
     const getPlaceDetails = new GetPlaceDetailsFake();
     const id = 'any_id';
-    const { result } = renderHook(() =>
-      usePlaceDetails({
-        gallerySummaryImages: [faker.image.imageUrl()],
-        getPlaceDetails,
-        id,
-      }),
-    );
+
+    const { result } = makeSut(id, [faker.image.imageUrl()], getPlaceDetails);
 
     await waitFor(() => {
       expect(result.current.amountOfReviews).toEqual(
@@ -200,16 +168,11 @@ describe('Presentation: usePlaceDetails', () => {
   });
 
   test('should get empty place details when GetPlaceDetails get returns error', async () => {
+    const id = 'any_id';
     const getPlaceDetails = new GetPlaceDetailsFake();
     getPlaceDetails.completeGetWithError('ocorreu um erro');
-    const id = 'any_id';
-    const { result } = renderHook(() =>
-      usePlaceDetails({
-        gallerySummaryImages: [faker.image.imageUrl()],
-        getPlaceDetails,
-        id,
-      }),
-    );
+
+    const { result } = makeSut(id, [faker.image.imageUrl()], getPlaceDetails);
 
     await waitFor(() => {
       expect(result.current.amountOfReviews).toEqual('');
@@ -225,3 +188,17 @@ describe('Presentation: usePlaceDetails', () => {
     });
   });
 });
+
+const makeSut = (
+  id: string,
+  gallerySummaryImages: Array<string> = [faker.image.imageUrl()],
+  getPlaceDetails: GetPlaceDetails = new GetPlaceDetailsFake(),
+) => {
+  return renderHook(() =>
+    usePlaceDetails({
+      gallerySummaryImages,
+      getPlaceDetails,
+      id,
+    }),
+  );
+};
