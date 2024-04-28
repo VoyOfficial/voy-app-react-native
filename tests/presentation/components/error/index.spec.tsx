6fc197ac95b1f16e0661834bdc5a96d17d289f5b
Marlon Marques <marlon.belohd@gmail.com>
2024-04-28 18:48:57 -0300

    test(presentation/error): show error content correctly

---
+++ tests/presentation/components/error/index.spec.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { render } from '@testing-library/react-native';
+
+describe('Components: Error', () => {
+  test('should show error content correctly', () => {
+    const content: ErrorContent = {
+      message: 'any_message',
+      title: 'any_title',
+    };
+    const { getByText } = render(<Error content={content} />);
+
+    expect(getByText(content.title)).toBeTruthy();
+    expect(getByText(content.message)).toBeTruthy();
+  });
+});
+
+type ErrorContent = {
+  title: string;
+  message: string;
+};
+
+type Props = {
+  content: ErrorContent;
+};
+
+const Error = ({ content }: Props) => {
+  const { title, message } = content;
+  return (
+    <>
+      <Text>{title}</Text>
+      <Text>{message}</Text>
+    </>
+  );
+};
