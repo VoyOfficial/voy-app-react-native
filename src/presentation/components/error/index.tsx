632dc51985f925e079e3b08f2aa2a71106edefb1
Marlon Marques <marlon.belohd@gmail.com>
2025-02-10 19:59:52 -0300

    style(presentation/error): update the style of the error component

--- src/presentation/components/error/index.tsx
+++ src/presentation/components/error/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Dimensions, Text, TouchableOpacity, View } from 'react-native';
 import Icon from '../../assets/fonts/Voy';
+
+const dimensions = Dimensions.get('screen');
 
 export type ErrorContent = {
   title: string;
@@ -12,26 +14,72 @@ type Props = {
   tryAgain: () => void;
 };
 
+const Title = ({ text }: { text: string }) => (
+  <Text
+    style={{
+      fontSize: 22,
+      padding: 4,
+      color: '#212121',
+      fontFamily: 'LexendDeca-Regular',
+    }}
+  >
+    {text}
+  </Text>
+);
+
+const Message = ({ text }: { text: string }) => (
+  <Text
+    style={{ fontSize: 16, color: '#212121', fontFamily: 'LexendDeca-Regular' }}
+  >
+    {text}
+  </Text>
+);
+
 const Error = ({ content, tryAgain }: Props) => {
   const { title, message } = content;
   return (
-    <>
-      <Icon testID="sad_icon_id" name="sad" />
+    <View
+      style={{
+        position: 'absolute',
+        backgroundColor: '#F1F5F6',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: dimensions.width,
+        height: dimensions.height,
+      }}
+    >
+      <Icon testID="sad_icon_id" name="sad" size={46} color="#212121" />
       {title && message ? (
-        <>
-          <Text>{title}</Text>
-          <Text>{message}</Text>
-        </>
+        <View
+          style={{
+            paddingVertical: 26,
+            alignItems: 'center',
+          }}
+        >
+          <Title text={title} />
+          <Message text={message} />
+        </View>
       ) : (
-        <>
-          <Text>{'Aaaah não'}</Text>
-          <Text>{'Parece que tivemos um imprevito.'}</Text>
-        </>
+        <View
+          style={{
+            paddingVertical: 26,
+            alignItems: 'center',
+          }}
+        >
+          <Title text={'Aaaah não'} />
+          <Message text={'Parece que tivemos um imprevito.'} />
+        </View>
       )}
-      <TouchableOpacity testID="button_try_again_id" onPress={tryAgain}>
-        <Text>{'Tente novamente'}</Text>
+      <TouchableOpacity
+        testID="button_try_again_id"
+        style={{ backgroundColor: '#5452F6', padding: 10, borderRadius: 10 }}
+        onPress={tryAgain}
+      >
+        <Text style={{ color: '#FFFFFF', fontFamily: 'LexendDeca-Regular' }}>
+          {'Tente novamente'}
+        </Text>
       </TouchableOpacity>
-    </>
+    </View>
   );
 };
 
