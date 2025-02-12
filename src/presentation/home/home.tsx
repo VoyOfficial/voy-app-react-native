8fb34eb59c94663e48385661da1b75e4d733ad05
Marlon Marques <marlon.belohd@gmail.com>
2025-02-12 17:28:05 -0300

    style(presentation/loading): update loading style

--- src/presentation/home/home.tsx
+++ src/presentation/home/home.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { ActivityIndicator, TouchableOpacity, View } from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { ListRecommendation } from '../recommendation/components';
 import CardList from '../components/cardList';
 import { Origin } from '../placeList/usePlaceList';
@@ -7,8 +12,23 @@ import Icon from '../assets/fonts/Voy';
 import { Error } from '../components';
 import { HomeViewModel } from './useHome';
 
+const dimensions = Dimensions.get('screen');
+
 const Loading = () => {
-  return <ActivityIndicator testID="loading_animation_id" />;
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        width: dimensions.width,
+        height: dimensions.height,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F1F5F6',
+      }}
+    >
+      <ActivityIndicator testID="loading_animation_id" size={'large'} />
+    </View>
+  );
 };
 
 const Home = ({
