87b1c72138d91ed0c019cdf147a56a1580ba6f85
Marlon Marques <marlon.belohd@gmail.com>
2025-08-23 17:35:31 -0300

    feat(main/homefactory): import remoteListRecommendations, remoteListPlaces

--- src/main/factories/presentation/homeFactory.tsx
+++ src/main/factories/presentation/homeFactory.tsx
@@ -1,37 +1,11 @@
 import React from 'react';
 import { RouteProp } from '@react-navigation/native';
-import { ListPlaces, ListRecommendations } from '~/domain/useCases';
-import { PlaceModel, RecommendationModel } from '~/domain/models';
 import { AxiosAdapter } from '~/infra/http';
+import { RemoteListPlaces, RemoteListRecommendations } from '~/data/useCases';
+import { FirebaseAnalyticsAdapter } from '~/infra/analytics';
 import { Actions, Routes, navigator } from '../../../../src/main/navigation';
 import { Home, useHome } from '../../../presentation/home';
 import { StackParams } from '../../../../src/main/navigation/navigation';
-
-class ListRecommendationsDAO implements ListRecommendations {
-  async list(): Promise<RecommendationModel[]> {
-    const axios = new AxiosAdapter();
-    const response = await axios.get({
-      url: 'http://localhost:3000/recommendations', //10.0.2.2:3000
-    });
-    return response.body;
-  }
-}
-
-class ListPlacesDAO implements ListPlaces {
-  constructor() {}
-  async list(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    location: { long: string; lat: string },
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    nextPageToken?: string | undefined,
-  ): Promise<PlaceModel[]> {
-    const axios = new AxiosAdapter();
-    const response = await axios.get({
-      url: 'http://localhost:3000/places', // 10.0.2.2:3000
-    });
-    return response.body;
-  }
-}
 
 type Props = {
   route: RouteProp<StackParams, Routes>;
@@ -41,8 +15,16 @@ type Props = {
 const HomeFactory = ({}: Props) => {
   const viewModel = useHome({
     navigate: new Actions(navigator).navigate,
-    listRecommendations: new ListRecommendationsDAO(),
-    listPlaces: new ListPlacesDAO(),
+    listRecommendations: new RemoteListRecommendations(
+      'http://localhost:3000/recommendations',
+      new AxiosAdapter(),
+      new FirebaseAnalyticsAdapter(),
+    ),
+    listPlaces: new RemoteListPlaces(
+      'http://localhost:3000/places',
+      new AxiosAdapter(),
+      new FirebaseAnalyticsAdapter(),
+    ),
   });
   return <Home {...viewModel} />;
 };
