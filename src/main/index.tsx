db75c962aadeb631ac0753e95b7f29a50ecf06a7
Sting <stingufc@gmail.com>
2025-09-17 16:33:06 -0300

    feat(global app): added global theme

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import styled from 'styled-components/native';
+import styled, { ThemeProvider } from 'styled-components/native';
 import { Provider } from 'mobx-react';
 import { NavigationContainerRef } from '@react-navigation/native';
+import { themes } from '../presentation/theme/theme';
 import Navigation, { StackParams } from './navigation/navigation';
 import { Routes, getScreensStack, setTopLevelNavigator } from './navigation';
 import stores from './stores';
@@ -19,15 +20,17 @@ const Main: React.FC<Props> = ({
   return (
     <WrapperScreen>
       <StatusBar barStyle={'dark-content'} />
-      <Provider {...stores}>
-        <Navigation
-          setNavigationTop={(navigationRef: NavigationContainerRef<any>) =>
-            setTopLevelNavigator(navigationRef)
-          }
-          initialRouteName={initialRouteName}
-          screensStack={screensStack}
-        />
-      </Provider>
+      <ThemeProvider theme={themes.light}>
+        <Provider {...stores}>
+          <Navigation
+            setNavigationTop={(navigationRef: NavigationContainerRef<any>) =>
+              setTopLevelNavigator(navigationRef)
+            }
+            initialRouteName={initialRouteName}
+            screensStack={screensStack}
+          />
+        </Provider>
+      </ThemeProvider>
     </WrapperScreen>
   );
 };
